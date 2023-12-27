2ca76b0854d0ab164c07f000848c7a552ed1ad24
Arhizmei <arhizmei@gmail.com>
2023-12-27 15:46:13 +0200

    another short decision with ternary operator

--- src/Alarm.java
+++ src/Alarm.java
@@ -1,5 +1,5 @@
 public class Alarm {
     public static boolean setAlarm(boolean employed, boolean vacation) {
-        return employed && !vacation ;
+        return employed & !vacation ? true: false ;
     }
 }
