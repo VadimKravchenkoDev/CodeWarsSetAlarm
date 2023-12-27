9cb859e129ab7cc0a68aee463fbe1ee02cd3fa8f
Arhizmei <arhizmei@gmail.com>
2023-12-27 15:27:28 +0200

    best decision

--- src/Alarm.java
+++ src/Alarm.java
@@ -1,11 +1,5 @@
 public class Alarm {
     public static boolean setAlarm(boolean employed, boolean vacation) {
-        if(employed&&vacation){
-            return false;
-        } else if(!employed&&!vacation){
-            return false;
-        } else if(!employed&&vacation){
-            return false;
-        } else return true;
+        return employed && !vacation ;
     }
 }
